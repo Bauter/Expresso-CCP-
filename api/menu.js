d5c0bb813361ded7b0a34edecee9d15808d6c933
Bauter <bauter09@gmail.com>
2020-12-03 22:04:24 -0600

    finished all routes in menu.js except for the delete route

--- api/menu.js
+++ api/menu.js
@@ -5,30 +5,136 @@ const menuItemRouter = require('./menu-item.js');
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-// Params
 
-// menuItems Router
+/* Params
+'''''''''*/
+
+menuRouter.param('menuId', (req, res, next, menuId) => {
+    const sql = `SELECT * FROM Menu WHERE Menu.id = $menuId`;
+    const values = {
+        $menuId: menuId
+    };
+
+    db.get(sql, values, (error, menu) => {
+        if (error) {
+            next(error);
+        } else if (menu) {
+            req.menu = menu;
+            next();
+        } else {
+            res.sendStatus(404);
+        };
+    });
+});
+
+
+/* menuItems Router
+'''''''''''''''''''*/
 
 menuRouter.use('/:menuId/menu-items', menuItemRouter);
+
 
 //=====================================
 // '/menus' ('/') path
 //=====================================
 
-// GET Route
 
-// POST Route
+/* GET Route
+''''''''''''*/
+
+menuRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM Menu`, (error, menu) => {
+        if(error) {
+            next(error);
+        } else {
+            return res.status(200).json({menu: menu});
+        };
+    });
+});
+
+/* POST Route
+'''''''''''''*/
+
+menuRouter.post('/', (req, res, next) => {
+
+    // Define and check that all required fields are present, if not, return 400 status code
+
+    const title = req.body.employee.title;
+
+    if(!title) {
+        return res.sendStatus(400);
+    };
+
+    // Define db query and values
+
+    const sql = `INSERT INTO Menu(title) VALUES ($title)`;
+    const values = {
+        $title: title
+    };
+
+    // Run the db query, get the updated response with "this.lastID"
+
+    db.run(sql, values, function(error) {
+        if(error) {
+            next(error);
+        } else {
+            db.get(`SELECT * FROM Menu WHERE Menu.id = ${this.lastID}`, (error, menu) => {
+                return res.status(201).json({menu: menu});
+            });
+        };
+    });
+});
 
 //=====================================
 // '/menus/:menuId' ('/:menusId') path
 //=====================================
 
-// GET Route
 
-// PUT Route
+/* GET Route
+''''''''''''*/
 
-// DELETE Route
+menuRouter.get('/:menuId', (req, res, next) => {
+    res.status(200).json({menu: req.menu});
+});
 
+
+/* PUT Route
+''''''''''''*/
+
+menuRouter.put('/:menuId', (req, res, next) => {
+
+    // Define and check that all required fields are present, if not, return 400 status code
+
+    const title = req.body.employee.title;
+
+    if(!title) {
+        return res.sendStatus(400);
+    };
+
+    // Define db query and values
+
+    const sql = `UPDATE Menu SET title = $title WHERE Menu.id = $menuId`;
+    const values = {
+        $title: title,
+        $menuId: req.params.menuId
+    };
+
+    // Run the db query, get the updated response
+
+    db.run(sql, values, (error) => {
+        if(error) {
+            next(error);
+        } else {
+            db.get(`SELECT * FROM Menu WHERE Menu.id = ${req.params.menuId}`, (error, menu) => {
+                return res.status(200).json({menu: menu});
+            });
+        };
+    });
+});
+
+
+/* DELETE Route
+'''''''''''''''*/
 
 
 
