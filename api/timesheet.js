262ff238b14e3b7ea380eaca208a527dd1137e95
Bauter <bauter09@gmail.com>
2020-12-03 22:10:37 -0600

    timesheet.js params set up

--- api/timesheet.js
+++ api/timesheet.js
@@ -4,6 +4,28 @@ const timesheetRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+
+/* Params
+'''''''''*/
+
+timesheetRouter.params('timesheetId', (req, res, next, timesheetId) => {
+    const sql = `SELECT * FROM Timesheet WHERE Timesheet.id = $timesheetId`;
+    const values = {
+        $timesheetId: timesheetId
+    };
+
+    db.get(sql, values, (error, timesheet) => {
+        if(error) {
+            next(error);
+        } else if (timesheet) {
+            next();
+        } else {
+            res.sendStatus(404);
+        };
+    });
+});
+
+
 //===================================
 // '/timesheets' ('/') path
 //===================================
